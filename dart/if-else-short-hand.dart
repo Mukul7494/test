3194adcf495c0cf85716c68ee338ca1179cffdcb
Mukul7494 <107314378+Mukul7494@users.noreply.github.com>
2022-06-25 14:48:17 +0530

    Delete dart directory

--- dart/if-else-short-hand.dart
+++
@@ -1,6 +0,0 @@
-void main()
-{
-    int a=5;
-    int b=8;
-    b>a ? print("B is Greater value") :print("A is Greater value");
-}
--- dart/odd-using-interpolation.dart
+++
@@ -1,5 +0,0 @@
-void main()
-{
-    var a=5;
-    a%2 == 0 ? print("$a is an Even number") : print("$a is an Odd number");
-}
